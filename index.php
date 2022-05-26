c7ce5a338b270ffcd1629edfbcb51534dd83cf50
edilsonpx <94394633+edilsonpx@users.noreply.github.com>
2022-05-26 20:41:47 -0300

    meusite223

---
+++ index.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<title>Variedades</title>
+	<link rel="stylesheet" href="estilo/font-awesome.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
+	<link href="estilo/style.css" rel="stylesheet"/>
+	<meta name="viewport" content="width=device-whith,initial-scale=1.0">
+	<meta name="keywordes" content="palavras-chaves,do,meu,site"> <meta name="description" content="Descrição do meu site"> 
+	 <meta charset="utf-8"/>
+<head>
+<body>
+
+	<header>
+		<div class="center">
+		<div class="logo left">Loja de Variedades de Qualidades</div><!--logo-->
+		<nav class="desktop right">
+			<ul>
+				<li><a href="">Home</a></li>
+				<li><a href="">Sobre</a></li>
+				<li><a href="">Serviços</a></li>
+				<li><a href="">Contato</a></li>
+			</ul>
+		</nav>
+		  <nav class="mobile right">
+		  	<div class="botao-menu-mobile">
+		  	<i class="fa fa-bars" aria-hidden="true"></i>
+		   </div>
+			<ul>
+				<li><a href="">Home</a></li>
+				<li><a href="">Sobre</a></li>
+				<li><a href="">Serviços</a></li>
+				<li><a href="">Contato</a></li>
+			</ul>
+		</nav>
+		<div class="clear"></div><!--clear-->
+		<div><!--center-->
+	</header>
+    
+	<section class="banner-principal">
+		<div class="overlay"></div><!--overlay-->
+        <div class="center">
+        <form>
+	     		<h2>qual o seu melhor e-mail?</h2>
+		    	<input type="email" name= "email" required />
+		    	<input type="submit" name= "ação" value="Cadastrar!">
+		</form>
+		<div><!--center-->
+	</section><!--banner-principal-->
+
+	<section class="descrição-Autor">
+		<div class="center">
+		<div class="w50 left">
+			<h2>Edilson da costa silva</h2>
+			<p>Esse povo tornou-se conhecido em todo o mundo pela sua cultura e tradição, pelos objetos de ouro que confeccionavam, pela utilização de pedras em suas obras tais quais estradas nas montanhas, canais de irrigação, etc.</p>
+			<p>Esse povo tornou-se conhecido em todo o mundo pela sua cultura e tradição, pelos objetos de ouro que confeccionavam, pela utilização de pedras em suas obras tais quais estradas nas montanhas, canais de irrigação, etc.</p>
+	</div><!--w50-->
+    <div class="w50 left">
+			<!--Pegar imagem depois-->
+			<img class="right" src="imagens/led.jpg" />
+	  </div><!--w50-->
+	  <div class="clear"></div>
+	<div><!--center-->
+</section><!--descrição-autor-->
+
+<section class="especialidades">
+
+  <div class="center">
+	<h2 class="tilte">especialidades</h2>
+		<div class="w33 left box-especialidade">
+			<h3><i class="fa fa-css3" aria-hidden="true"></i></h3>
+			<h3>javaScript</h3>
+			<p>A capital do Império Inca chamava-se Cuzco, e lá havia o maior templo de culto ao deus Sol, o principal deus da religião inca</p>
+
+</section><!--box-especialidade-->
+<div class="w33 left box-especialidade">
+			<h3><i class="fa fa-html5" aria-hidden="true"></i></h3>
+			<h3>HTML5</h3>
+			<p>A capital do Império Inca chamava-se Cuzco, e lá havia o maior templo de culto ao deus Sol, o principal deus da religião inca</p>
+
+
+</section><!--box-especialidade-->
+<div class="w33 left box-especialidade">
+			<h3><i class="fa fa-gg-circle" aria-hidden="true"></i></h3>
+			<h3>CSS3</h3>
+			<p>A capital do Império Inca chamava-se Cuzco, e lá havia o maior templo de culto ao deus Sol, o principal deus da religião inca</p>
+
+
+</div><!--box-especialidade-->
+</div><!--center-->
+</section><!--especialidades-->
+
+<section class="extras">
+
+
+	<div class="center">
+		<div class="w50 left">
+		
+			<h2 class="titlte">Depoimentos</h2>
+			<div class="depoimento-sigle">
+				<p class="depoimento-descrição">Os ratos domésticos comercializados como animais de estimação geralmente possuem um tamanho diferente do rato comum. Isso é atribuído tanto à criação quanto a diferentes condições na natureza. O mais conhecido entre eles,</p>
+				<p class="nome-Autor">Lorem Ipsum</p>
+			</div><!--depoimento-single-->
+				<div class="depoimento-sigle">
+			<p class="depoimento-descrição">Os ratos domésticos comercializados como animais de estimação geralmente possuem um tamanho diferente do rato comum. Isso é atribuído tanto à criação quanto a diferentes condições na natureza. O mais conhecido entre eles, o famoso rato branco de laboratório, possui características mais uniformes,</p>
+				<p class="nome-Autor">Lorem Ipsum</p>
+			</div><!--depoimento-single-->
+			<div class="depoimento-sigle">
+			<p class="depoimento-descrição">Os ratos domésticos comercializados como animais de estimação geralmente possuem um tamanho diferente do rato comum.  </p>
+				<p class="nome-Autor">Lorem Ipsum</p>
+			</div><!--depoimento-single-->
+		</div><!--w50-->
+		<div class="w50 left">
+			<h2 class="title">Serviços</h2>
+			<div class="serviços">
+			<ul>
+				<li>Os ratos domésticos comercializados como animais de estimação geralmente possuem um tamanho diferente do rato comum. Isso é atribuído tanto à criação quanto a diferentes condições na natureza. O mais conhecido entre eles,</li>
+				
+
+				<div class="center">
+		<p>Todos os direitos reservados</p>
+		</div><!--center-->
+			</ul>
+      </div><!--serviços-->
+		</div><!--w50-->
+		<div class="clear"></div>
+	</div><!--center-->
+</section><!--extras-->
+
+
+<footer>
+<div class="center">
+		<p>Todos os direitos reservados</p>
+		</div><!--center-->
+
+</footer>
+
+</body>
+</html>
